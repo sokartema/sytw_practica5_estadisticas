553b2cc8ea9da1d5672384eac91bffee3a5a6318
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-11-06 15:02:56 +0000

    Editando archivo yml, uniendo test de selenium y creando archivo de test unitarios

--- test/test_u.rb
+++ test/test_u.rb
@@ -47,7 +47,7 @@ describe "Tests de la pagina raiz principal ('/') con metodo get" do
 	it "Comprueba la carga del logo en el public del servidor" do
 		path = File.absolute_path(__FILE__)
 		path=path+@imagen
-		path=path.split('/test/test.rb')
+		path=path.split('/test/test_u.rb')
 		path=path[0]+path[1]
 
 		assert File.exists?(path), "Debe estar la imagen en public"
@@ -58,7 +58,7 @@ describe "Tests de la pagina raiz principal ('/') con metodo get" do
 
 		path = File.absolute_path(__FILE__)
 		path=path+@css
-		path=path.split('/test/test.rb')
+		path=path.split('/test/test_u.rb')
 		path=path[0]+path[1]
 
 		assert File.exists?(path), "Debe estar el CSS en el servidor"
@@ -135,80 +135,4 @@ describe "Test para datamapper" do
 		assert(prueba.destroy)
 	end
 end
-
-describe "Test de la paginas de login" do
-	before :all do
-		@pagina='/usr/google'
-		@pagina2='/usr/twitter'
-		@pagina3='/usr/facebook'
-		@textocontenido="Una sola cuenta de Google para todos los servicios de Google"
-		@textocontenido3='Entrar en Facebook'
-		@textocontenido2='Condiciones de Servicio de Twitter'
-
-	end
-
-	it "Logueo de Google" do
-
-		browser = Selenium::WebDriver.for :firefox
-		browser.get('localhost:9292')
-		browser.manage.timeouts.implicit_wait=3
-		element=browser.find_element :id => "googleboton"
-		element.click
-		browser.manage.timeouts.implicit_wait=3
-
-		body_element = browser.find_element(:tag_name => "body")
-		body_element=body_element.text.to_s
-		
-		value=false
-		if body_element.include? "Una sola cuenta de Google para todos los servicios de Google"
-			value=true
-		end
-		assert (value)
-		browser.close()
-
-	end
-
-	it "Logueo de Twitter" do
-
-		browser = Selenium::WebDriver.for :firefox
-		browser.get('localhost:9292')
-		browser.manage.timeouts.implicit_wait=3
-		element=browser.find_element :id => "twitterboton"
-		element.click
-		browser.manage.timeouts.implicit_wait=3
-
-		body_element = browser.find_element(:tag_name => "body")
-		body_element=body_element.text.to_s
-		
-		value=false
-		if body_element.include? "Condiciones de Servicio de Twitter"
-			value=true
-		end
-		assert (value)
-		browser.close()
-	end
-	it "Logueo de Facebook" do
-
-		browser = Selenium::WebDriver.for :firefox
-		browser.get('localhost:9292')
-		browser.manage.timeouts.implicit_wait=3
-		element=browser.find_element :id => "facebookboton"
-		element.click
-		browser.manage.timeouts.implicit_wait=3
-		body_element = browser.find_element(:tag_name => "body")
-		body_element=body_element.text.to_s
-		
-		value=false
-		if body_element.include? "Entrar en Facebook"
-			value=true
-		end
-		assert (value)
-		browser.close()
-	end
-	
 end
-
-
-
-end
-
